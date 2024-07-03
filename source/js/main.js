0baac6714c768bd1e3df0a2e272eabf61eed7191
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-07-03 16:58:27 +0300

    Сверстал мобильную версию about

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,19 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+window.addEventListener('load', () => {
+  const uploadVideoButton = document.querySelector('.about__play');
+  const preview = document.querySelector('.about__preview');
+  const previewWrapper = document.querySelector('.about__preview-wrapper');
+  uploadVideoButton.addEventListener('click', () => {
+    preview.remove();
+    uploadVideoButton.remove();
+    const videoIframe = document.createElement('iframe');
+    videoIframe.classList.add('about__video');
+    videoIframe.src = 'https://www.youtube.com/embed/9TZXsZItgdw?si=tJpCEikTAfWTN1tk';
+    videoIframe.frameBorder = 0;
+    previewWrapper.appendChild(videoIframe);
+    previewWrapper.classList.add('about__preview-wrapper--no-background');
+  });
+});
