02928e2f4438421135c2270f07e0e19ad02f6560
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-07-04 12:33:01 +0300

    Запрограммировал табы

--- source/js/main.js
+++ source/js/main.js
@@ -4,3 +4,16 @@
 // import 'swiper/css';
 import './video';
 
+window.addEventListener('load', () => {
+  const tabs = document.querySelector('.price__tabs');
+  tabs.addEventListener('click', (e) => {
+    const oldTab = document.querySelector('.price__tab--active');
+    oldTab.classList.remove('price__tab--active');
+    e.target.classList.add('price__tab--active');
+    const tabNumber = e.target.dataset.open;
+    const oldActiveContent = document.querySelector('.price__tab-content--active');
+    oldActiveContent.classList.remove('price__tab-content--active');
+    const newActiveContent = document.querySelector(`[data-tab="${tabNumber}"]`);
+    newActiveContent.classList.add('price__tab-content--active');
+  });
+});
