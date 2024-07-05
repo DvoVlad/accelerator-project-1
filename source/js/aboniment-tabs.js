b2e316f3e1b5b99fa44f8be94aabf9f1fcda3375
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-07-05 15:52:45 +0300

    Вынес в модуль скрипт таба

---
+++ source/js/aboniment-tabs.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', () => {
+  const tabs = document.querySelector('.price__tabs');
+  tabs.addEventListener('click', (e) => {
+
+    if(!e.target.closest('.price__tab')) {
+      return;
+    }
+    const oldTab = document.querySelector('.price__tab--active');
+    oldTab.classList.remove('price__tab--active');
+    e.target.classList.add('price__tab--active');
+    const tabNumber = e.target.dataset.open;
+    const oldActiveContent = document.querySelector('.price__tab-content--active');
+    oldActiveContent.classList.remove('price__tab-content--active');
+    const newActiveContent = document.querySelector(`[data-tab="${tabNumber}"]`);
+    newActiveContent.classList.add('price__tab-content--active');
+  });
+});
--- source/js/main.js
+++ source/js/main.js
@@ -3,21 +3,5 @@
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
 import './video';
+import './aboniment-tabs';
 
-window.addEventListener('load', () => {
-  const tabs = document.querySelector('.price__tabs');
-  tabs.addEventListener('click', (e) => {
-
-    if(!e.target.closest('.price__tab')) {
-      return;
-    }
-    const oldTab = document.querySelector('.price__tab--active');
-    oldTab.classList.remove('price__tab--active');
-    e.target.classList.add('price__tab--active');
-    const tabNumber = e.target.dataset.open;
-    const oldActiveContent = document.querySelector('.price__tab-content--active');
-    oldActiveContent.classList.remove('price__tab-content--active');
-    const newActiveContent = document.querySelector(`[data-tab="${tabNumber}"]`);
-    newActiveContent.classList.add('price__tab-content--active');
-  });
-});
